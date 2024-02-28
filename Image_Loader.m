ae62cab8766b31b6628513738b13e200e3d091ba
Daniel Pitzele <dpitzele@gmail.com>
2024-02-28 18:45:50 -0500

    fixed spacing

--- Image_Loader.m
+++ Image_Loader.m
@@ -11,7 +11,6 @@ full_directory = fullfile(directory, image_name);
 % Load in image
 img_RGB = imread(full_directory);
 
-
 % sRGB to linear RGB
 img_RGB = rgb2lin(img_RGB);
 img_RGB = im2double(img_RGB);
@@ -25,6 +24,7 @@ xyz_to_lms = [0.38971 0.68898 -0.07868;
               -0.22981 1.18340 0.04641;
               0 0 1];
 
+% LMS shift (or map to 0)
 size1 = size(img_RGB,1);
 size2 = size(img_RGB,2);
 img_LMS = zeros(size1,size2,3);
@@ -32,7 +32,7 @@ mod_XYZ = zeros(size1,size2,3);
 for x = 1:size1
     for y = 1:size2
         img_LMS(x,y,:) = xyz_to_lms * squeeze(img_XYZ(x,y,:));
-        img_LMS(x,y,1) = 0;
+        img_LMS(x,y,3) = 0;
         mod_XYZ(x,y,:) = squeeze(img_LMS(x,y,:));
     end
 end
