cadc8220d755564a804e24c817c3e898bb0419eb
Daniel Pitzele <dpitzele@gmail.com>
2024-11-11 16:24:00 -0500

    Combined solutions to test on images

--- RGB_To_OppCVD_Matrix.m
+++ RGB_To_OppCVD_Matrix.m
@@ -1,9 +1,12 @@
+% Testing the construction of a matrix that moves data from RGB all the way
+% to colorblind opponent space. Tests this on real images.
+
 % File name
-clear all;
 % image_name = "forest.jpg";
-image_name = "butterflies.jpg";
-red_shift = 0;
-green_shift = 10;
+% image_name = "butterflies.jpg";
+image_name = "color_wheel.png";
+red_shift = -10;
+green_shift = 0;
 
 % Directory to file
 % directory = "C:\Users\YOUR USER HERE\Desktop\test_images";
@@ -29,7 +32,7 @@ m_new_data = interp1(wavelength_data, m_data, desired_wavelengths, 'cubic');
 s_new_data = interp1(wavelength_data, s_data, desired_wavelengths, 'cubic');
 % LMS_new_cmf = [desired_wavelengths, l_new_data, m_new_data, s_new_data];
 
-%% Shift LMS response data (apply colorblindness)
+%% Calculate shifting matrix / shift LMS response data
 % if red_shift ~= 0
 %     for i = 1:(size(l_new_data) + red_shift)
 %         l_new_data(i) = l_new_data(i - red_shift);
@@ -102,16 +105,88 @@ for i = 1:length(s_new_data)
     opp_new_data(i,:) = paperMatrix * [l_cvd(i); m_cvd(i); s_new_data(i)];
 end
 
-% plotting opponent space data after shift
-% figure
-% hold on
-% plot(390:830, opp_new_data);
-% xlim([390 830]);
+%% Element-wise multiplication to simulate integration
 
-% plotting cone response data after shift
+x_data = XYZ_cmf{:,2};      % x column data.
+y_data = XYZ_cmf{:,3};      % y column data.
+z_data = XYZ_cmf{:,4};      % z column data.
+
+% normalize relative to RGB CMFs
+% sum_cmfs = x_data + y_data + z_data;
+% x_data = x_data ./ sum_cmfs;
+% y_data = y_data ./ sum_cmfs;
+% z_data = z_data ./ sum_cmfs;
+
+% X point integrals
+XL_int = sum(x_data .* l_cvd);
+XM_int = sum(x_data .* m_cvd);
+XS_int = sum(x_data .* s_new_data);
+
+% Y point integrals
+YL_int = sum(y_data .* l_cvd);
+YM_int = sum(y_data .* m_cvd);
+YS_int = sum(y_data .* s_new_data);
+
+% Z point integrals
+ZL_int = sum(z_data .* l_cvd);
+ZM_int = sum(z_data .* m_cvd);
+ZS_int = sum(z_data .* s_new_data);
+
+
+%% Normalization
+sum_Ls = XL_int + YL_int + ZL_int;
+sum_Ms = XM_int + YM_int + ZM_int;
+sum_Ss = XS_int + YS_int + ZS_int;
+
+XL_int = XL_int / sum_Ls;
+YL_int = YL_int / sum_Ls;
+ZL_int = ZL_int / sum_Ls;
+XM_int = XM_int / sum_Ms;
+YM_int = YM_int / sum_Ms;
+ZM_int = ZM_int / sum_Ms;
+XS_int = XS_int / sum_Ss;
+YS_int = YS_int / sum_Ss;
+ZS_int = ZS_int / sum_Ss;
+
+xyz2cvd_lms = [XL_int, YL_int, ZL_int;
+               XM_int, YM_int, ZM_int;
+               XS_int, YS_int, ZS_int];
+
+img_RGB = imread(full_directory);
+% sRGB to linear RGB
+img_RGB = rgb2lin(img_RGB);
+img_RGB = im2double(img_RGB);
+
+
+
+%% RGB to XYZ (CIE 1931 edition)
+img_XYZ = rgb2xyz(img_RGB);
+
+
+% XYZ to LMS
+% wikipedia
+% xyz_to_lms = [0.38971 0.68898 -0.07868;
+%               -0.22981 1.18340 0.04641;
+%               0 0 1];
+xyz_to_lms = [0.4260 0.5151 0.0589;
+              0.3289 0.5548 0.1164;
+              0.1486 0.0414 0.8099];
+
+%% Apply CVD shift to image
+size1 = size(img_RGB,1);
+size2 = size(img_RGB,2);
+img_LMS = zeros(size1,size2,3);
+mod_XYZ = zeros(size1,size2,3);
+for x = 1:size1
+    for y = 1:size2
+        img_LMS(x,y,:) = xyz2cvd_lms * squeeze(img_XYZ(x,y,:));
+        % disp(img_LMS(x,y,:))
+        % img_LMS(x,y,2) = 0;
+        mod_XYZ(x,y,:) = (xyz_to_lms^-1) * squeeze(img_LMS(x,y,:));
+    end
+end
+
+%% going back
+mod_RGB = xyz2rgb(mod_XYZ);
 figure
-hold on
-plot(desired_wavelengths, l_cvd, 'r')
-plot(desired_wavelengths, m_cvd, 'g')
-plot(desired_wavelengths, s_new_data, 'b')
-xlim([390 830]);
+imshowpair(img_RGB,mod_RGB,'montage')
