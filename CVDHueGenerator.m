04b33e0efc21cea743e65d35f95e18b25e9b15a3
Channing04 <pridgeon.channing@gmail.com>
2025-09-15 21:21:03 -0400

    Added Script for CVD hue generation

---
+++ CVDHueGenerator.m
@@ -0,0 +1,71 @@
+clc; clear; close all;
+
+%Enter red or green shift
+red_shift = -15; % negative
+green_shift = 0; % positive
+
+%Color Shifting matrix
+rgb2opp_cvd = gen_rgb2opp_mat(red_shift, green_shift);
+
+opp2rgb = gen_rgb2opp_mat(0, 0)^-1;
+
+rgb2rgb_cvd = opp2rgb * rgb2opp_cvd;
+%Read the csv values
+hue_data = readtable('data_tables/munsell_hex_40.csv', 'Delimiter', ',');
+hex_colors = hue_data{:,2};
+
+% disp(hex_colors);
+%Convert to RGB and Hex
+cvd_rgb = zeros(size(hue_data, 1), 3);
+cvd_hex = strings(size(hue_data, 1), 1);
+
+new_rgb = zeros(size(hex_colors, 1), 3);
+new_hex = strings(size(hex_colors, 1), 1);
+
+shiftHue = zeros(40, 1); 
+
+
+for i = 1:length(hex_colors)
+ % Convert hex to RGB (0-1)
+    hex_str = strtrim(string(hex_colors(i,:))); % Converts row to string
+    
+    % disp(hex_str);
+    
+    % if startsWith(hex_str, '#')
+    %     hex_str = extractAfter(hex_str, 1);
+    % end
+    % if strlength(hex_str) == 6
+    %     rgb = [hex2dec(hex_str(1:2)), hex2dec(hex_str(3:4)), hex2dec(hex_str(5:6))] / 255;
+    % else
+    %     error('Invalid hex string: %s', hex_str);
+    % end
+    % rgb = [hex2dec(hex_str(1:2)), hex2dec(hex_str(3:4)), hex2dec(hex_str(5:6))] / 255;
+    rgb = sscanf(hex_str, '%2x%2x%2x', [1 3]) / 255;
+
+    % disp(rgb);
+    % Apply the color shift matrix
+    shifted_rgb = (rgb2rgb_cvd * rgb')';
+    % Clip values to [0,1]
+    shifted_rgb = max(0, min(1, shifted_rgb));
+    shifted_rgb = reshape(shifted_rgb, 1, 3);
+    disp(size(shifted_rgb));
+
+    % Store new RGB and hex
+    new_rgb(i, :) = shifted_rgb;
+
+    new_shift = round(shifted_rgb .* 255);
+    new_hex(i) = sprintf('%02X%02X%02X', new_shift);
+    new_hex(i)
+    shiftHue(i, :) = new_hex(i);
+end
+
+%write new values into munsell_hex_shifted.csv
+hue_data.NewRGB = new_rgb;
+hue_data.NewHex = new_hex;
+
+
+% disp(new_hex);
+
+writematrix(new_hex, 'data_tables/munsell_hex_shifted.csv');
+% writetable(shiftHue, 'data_tables/munsell_hex_shifted.csv');
+
